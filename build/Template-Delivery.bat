571c859fe9373534d597b2ad631d072747b65954
clark159 <clark159@hotmail.com>
2024-04-26 11:36:46 +0800

    [nuget] Release MDP.Templates 8.0.2

--- build/Template-Delivery.bat
+++ build/Template-Delivery.bat
@@ -4,7 +4,7 @@ setlocal enabledelayedexpansion
 
 :: ================================================
 :: Variables
-set version=6.1.13
+set version=8.0.2
 
 set buildDir=%~dp0
 set srcDir=%~dp0..\templates
